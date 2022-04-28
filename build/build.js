621034328b789de3ba9a13929b62bced60a1dc44
umamaheswari.prakash <umamaheswari.prakash@syncfusion.com>
2022-04-28 16:15:41 +0530

    feature(REPORT-11350): Gulp-version-update

--- build/build.js
+++ build/build.js
@@ -2,23 +2,25 @@ const gulp = require("gulp");
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const shelljs = require('shelljs');
-const runSequence = require('run-sequence');
+const runSequence = require('gulp4-run-sequence');
 
-gulp.task('build', function (callback) {
-    runSequence('clean', 'production', 'copy-files', callback);
+gulp.task('build', (done) =>{
+    runSequence('clean', 'production', 'copy-files', done);
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', (done) => {
     shelljs.rm('-rf', 'dist');
     shelljs.rm('-rf', 'demos');
+    done();
 });
 
-gulp.task('copy-files', function () {
+gulp.task('copy-files', (done) => {
     shelljs.mkdir('-p', 'dist');
     shelljs.cp('-r', ['index.html', 'favicon.ico', 'assets', 'src', 'scripts'], 'dist');
+    done();
 })
 
-gulp.task('production', function () {
+gulp.task('production',() =>{
     return gulp.src('.')
         .pipe(webpackStream(require('./../webpack.config.js'), webpack))
         .pipe(gulp.dest('dist'));
--- build/serve.js
+++ build/serve.js
@@ -4,8 +4,10 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./../webpack.config.js');
 const open = require('open');
 const port = 9000;
+const runSequence = require('gulp4-run-sequence');
 
-gulp.task('serve', ['clean'], function () {
+gulp.task('serve', ()=> {
+  runSequence('clean',function(){
   const server = new WebpackDevServer(webpack(config));
   server.listen(port, 'localhost', function (err) {
     if (err) {
@@ -14,5 +16,5 @@ gulp.task('serve', ['clean'], function () {
     console.log('Reports JS sample browser is listening on localhost:', port);
     open('http://localhost:' + port + '/');
   });
-
+});
 });
