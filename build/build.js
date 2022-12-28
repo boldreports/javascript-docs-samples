28bc02bf66a020ab1f505f1c8f8455906a7816c1
Sridhar Manikandan <sridhar.manikandan@syncfusion.com>
2022-12-28 16:21:50 +0530

    feature(REPORT-19753): node14-and-gulp-4-update

--- build/build.js
+++ build/build.js
@@ -2,20 +2,22 @@ const gulp = require("gulp");
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const shelljs = require('shelljs');
-const runSequence = require('run-sequence');
+const runSequence = require('gulp4-run-sequence');
 
-gulp.task('build', function (callback) {
-    runSequence('clean', 'production', 'copy-files', callback);
+gulp.task('build', function (done) {
+    runSequence('clean', 'production', 'copy-files', done);
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function (done) {
     shelljs.rm('-rf', 'dist');
     shelljs.rm('-rf', 'demos');
+    done();
 });
 
-gulp.task('copy-files', function () {
+gulp.task('copy-files', function (done) {
     shelljs.mkdir('-p', 'dist');
     shelljs.cp('-r', ['index.html', 'favicon.ico', 'assets', 'src', 'scripts'], 'dist');
+    done();
 })
 
 gulp.task('production', function () {
