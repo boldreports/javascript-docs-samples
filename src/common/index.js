078cb7aa7f2717b83002a3728ff83efbf4f1a460
SundaresanGunasekaran <sundaresan.gunasekaran@syncfusion.com>
2025-09-17 13:03:51 +0530

    feature(REPORT-461761): Bold Reports 11.1 Release Changes

--- src/common/index.js
+++ src/common/index.js
@@ -3,12 +3,10 @@ window['$'] = jquery;
 import './index.css';
 
 //report
-import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';
-import '@boldreports/javascript-reporting-controls/Scripts/bold.report-designer.min';
-
-//data-visualization
-import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
-import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min';
+import '@boldreports/javascript-reporting-controls/Scripts/v2.0/common/bold.reports.common.min';
+import '@boldreports/javascript-reporting-controls/Scripts/v2.0/common/bold.reports.widgets.min';
+import '@boldreports/javascript-reporting-controls/Scripts/v2.0/bold.report-viewer.min';
+import '@boldreports/javascript-reporting-controls/Scripts/v2.0/bold.report-designer.min';
 
 import 'bootstrap';
 
